23def476af7a082bcfbf8341c64d2aba6609effa
Subhra Pratim Mondal <msubhra364@gmail.com>
2024-08-14 00:30:00 +0530

    first commit

---
+++ src/components/Credit.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      , <a href="https://github.com/shemmee/React-Weather-App">Open sourced on Github.</a> Hosted on Netlify
+    </p>
+  </div>
+  );
+}
